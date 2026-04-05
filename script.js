2fa01042eeba0a9099828200b072995ad79dddd3
thanhkimhuynh7749-spec <thanhkimhuynh7749@gmail.com>
2026-04-05 18:47:38 +0700

    Translate timer app UI elements to Vietnamese

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ addBtn.onclick = () => {
   timerDiv.className = "timer-app";
 
   timerDiv.innerHTML = `
-    <input class="name-input" placeholder="Tên timer">
+    <div style="font-weight:bold; margin-bottom:5px;">
+      YUS DEBATING TOURNAMENT
+    </div>
+
+    <input class="name-input" placeholder="Tên đồng hồ">
 
     <div class="timer-display">00:00</div>
 
-    <input class="time-input" type="number" placeholder="Nhập giây">
+    <input class="time-input" type="number" placeholder="Nhập số giây">
 
     <div class="buttons-container">
-      <button class="start">Start</button>
-      <button class="pause">Pause</button>
-      <button class="reset">Reset</button>
-      <button class="delete">X</button>
+      <button class="start">Bắt đầu</button>
+      <button class="pause">Tạm dừng</button>
+      <button class="reset">Khởi động lại</button>
+      <button class="delete">Xóa</button>
     </div>
   `;
 
@@ -56,7 +60,6 @@ addBtn.onclick = () => {
         clearInterval(interval);
         interval = null;
 
-        // 🔊 phát âm thanh
         sound.currentTime = 0;
         sound.play();
 
